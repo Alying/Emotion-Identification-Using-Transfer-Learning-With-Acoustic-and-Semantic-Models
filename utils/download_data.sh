952e5ffce32a0bab5d097e063c97bc5320c393dd
pepe <pepe@Speech.myguest.virtualbox.org>
2023-03-27 21:37:19 -0700

    only download git

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -4,21 +4,12 @@
 mkdir -p local/data
 cd local/data   ### Note: the rest of this script is executed from the directory 'db'.
 
-debug_mode=true
-
 # TED-LIUM database:
 if [ ! -e CREMA-D ]; then
   echo "$0: cloning the CREMA-D data (it won't re-download if it was already downloaded.)"
   # the following command won't re-get it if it's already there
   # because of the --continue switch.
- 
-  if $debug_mode; then
-  	mkdir -p CREMA-D
-	wget --continue https://github.com/CheyneyComputerScience/CREMA-D/archive/refs/heads/master.zip || exit 1
-  	unzip master.zip -d CREMA-D 
-  else
-     	git lfs clone https://github.com/CheyneyComputerScience/CREMA-D.git
-  fi
+  git lfs clone https://github.com/CheyneyComputerScience/CREMA-D.git
 else
   echo "$0: not downloading or unzipping CREMA-D because it already exists."
 fi
