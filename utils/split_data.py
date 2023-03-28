3d309a1c27679abcfa4d7850aeb681a2ced2a5ef
Alying <alina.ying@gmail.com>
2023-03-27 23:07:04 -0700

    basics of getting emotion label data

--- utils/split_data.py
+++ utils/split_data.py
@@ -1,7 +1,32 @@
 import os
+import random
 import pandas as pd
 from sklearn.model_selection import train_test_split
 cwd = os.getcwd()
 
+def tied_emotions(row):
+    emotions = str.split(row, ':')
+
+    # normal single emotion
+    if len(emotions) < 2:
+        return emotions[0]
+
+    # ignore Neutrals in tied emotions
+    try:
+        emotions.remove('N')
+    except:
+        True
+
+    # pick random emotion among remaining emotions
+    return random.choice(emotions)
+
+# read in csv
+df = pd.read_csv("../local/data/CREMA-D/processedResults/tabulatedVotes.csv")
+
+df['emoVote'] = df['emoVote'].apply(tied_emotions)
+print(df.emoVote.head(15))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.2)
 print(cwd)
 print("HELLO")
+
