43f92f2f63029be18d5c57ad20b2b644d6395c5e
Alying <alina.ying@gmail.com>
2023-04-01 15:16:36 -0700

    added download IEMOCAP code

--- run.sh
+++ run.sh
@@ -15,6 +15,6 @@ if [ $stage -le 1 ]; then
   utils/prepare_data.sh
 fi
 
-if [ $stage -le 2 ]; then
-  utils/make_mfcc.sh
-fi
+#if [ $stage -le 2 ]; then
+#  utils/make_mfcc.sh
+#fi
--- utils/download_data.sh
+++ utils/download_data.sh
@@ -4,12 +4,18 @@
 mkdir -p local/data
 cd local/data   ### Note: the rest of this script is executed from the directory 'db'.
 
-# TED-LIUM database:
+# CREMA-D database:
 if [ ! -e CREMA-D ]; then
-  echo "$0: cloning the CREMA-D data (it won't re-download if it was already downloaded.)"
-  # the following command won't re-get it if it's already there
-  # because of the --continue switch.
+  echo "$0: cloning the CREMA-D data" 
   git lfs clone https://github.com/CheyneyComputerScience/CREMA-D.git
+else
+  echo "$0: not downloading or unzipping CREMA-D because it already exists."
+fi
+
+# IEMOCAP database:
+if [ ! -e IEMOCAP ]; then
+  echo "$0: downloading the IEMOCAP data (it won't re-download if it was already downloaded.)"
+  tar -xf "IEMOCAP_full_release_withoutVideos.tar.gz"
 else
   echo "$0: not downloading or unzipping CREMA-D because it already exists."
 fi
