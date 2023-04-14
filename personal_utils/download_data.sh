bd4c5b82bd3479465a7e16856f41610994090b70
Alina Ying <alina.ying@gmail.com>
2023-04-14 04:30:12 +0000

    added automation for unzipping Tedlium pretrained model and putting it in the correct place

--- personal_utils/download_data.sh
+++ personal_utils/download_data.sh
@@ -21,9 +21,14 @@ else
 fi
 
 # Tedlium database:
-if [ ! -e Tedlium ]; then
+if [ ! -e Tedlium.tar.gz ]; then
   echo "$0: downloading the Tedlium data (it won't re-download if it was already downloaded.)"
-  gsutil cp -r gs://tedlium_with_pitch_model/pretrained_tedlium_dir.tar.gz local/data/Tedlium
+  gsutil cp -r gs://tedlium_with_pitch_model/pretrained_tedlium_dir.tar.gz Tedlium.tar.gz
+  echo "unzipping Tedlium"
+  tar -xvzf "Tedlium.tar.gz"
+  echo "unzipped Tedlium"
+  mv kaldi-trunk/egs/tedlium/s5_r3/exp/chain_cleaned_1d/tdnn1d_sp .
+  echo "moved Tedlium"
 else
   echo "$0: not downloading or unzipping Tedlium because it already exists."
 fi
