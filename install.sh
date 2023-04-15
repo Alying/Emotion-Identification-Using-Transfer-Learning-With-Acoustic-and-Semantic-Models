2fcdfc92098b9353340e6aee9d6356204f063b61
pepe <pepe@Speech.myguest.virtualbox.org>
2023-04-14 23:18:04 -0700

    can run bert now

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ sudo apt-get install python3-sklearn
 sudo apt install python3-pip
 # pip install tensorflow==1.11.0.
 # pip install numpy
-pip install torch
+# pip install torch
+# pip install future
+sudo pip3 install --upgrade pip
+sudo pip3 install setuptools-rust
+sudo -H pip3 install --ignore-installed PyYAML
+sudo pip3 install transformers
+sudo pip3 install torch
+sudo pip3 install -U Pillow
