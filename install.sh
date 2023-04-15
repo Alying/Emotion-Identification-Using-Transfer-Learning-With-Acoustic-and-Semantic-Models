2201a9c428c9a9e421af791fe8916460e57834a1
pepe <pepe@Speech.myguest.virtualbox.org>
2023-04-14 23:01:14 -0700

    bert

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 sudo apt-get install git-lfs
 sudo apt install python3-pandas
 sudo apt-get install python3-sklearn
+sudo apt install python3-pip
+# pip install tensorflow==1.11.0.
+# pip install numpy
+pip install torch
--- run2.sh
+++ run2.sh
@@ -2,6 +2,5 @@
 . ./path.sh || exit 1
 . ./cmd.sh || exit 1
 
-steps/make_mfcc_pitch.sh --nj 1 --cmd "$train_cmd" local/data/train local/data/logs local/data
 steps/make_mfcc_pitch.sh --nj 30 --cmd "$train_cmd" local/data/train
 steps/compute_cmvn_stats.sh local/data/train
