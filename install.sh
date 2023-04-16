37fd06d5a29e4d910578512c9b6c70e01aee8ac2
Alina Ying <alina.ying@gmail.com>
2023-04-16 00:07:33 +0000

    getting ertTokenizer + add install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt update
 sudo apt-get install git-lfs
 sudo apt install python3-pandas
 sudo apt-get install python3-sklearn
--- personal_utils/run_bert.py
+++ personal_utils/run_bert.py
@@ -1,2 +1,6 @@
 import torch
 from transformers import BertTokenizer, BertModel
+import logging
+import matplotlib.pyplot as plt
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
