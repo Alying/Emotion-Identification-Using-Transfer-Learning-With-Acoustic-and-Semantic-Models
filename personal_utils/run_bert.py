29b64f6a4d7229d02fa7bfbbd745eb9aeac7afb1
Alina Ying <alina.ying@gmail.com>
2023-04-16 02:09:57 +0000

    running single sentence through BERT model; still need to get output

--- personal_utils/run_bert.py
+++ personal_utils/run_bert.py
@@ -4,3 +4,29 @@ import logging
 import matplotlib.pyplot as plt
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+with open('local/data/test/text', 'r') as f:
+    for line in f:
+        print(repr(line))
+        marked_text = "[CLS] " + line + " [SEP]"
+
+        # Tokenize our sentence with the BERT tokenizer; split sentence into tokens
+        tokenized_text = tokenizer.tokenize(marked_text)
+        print(tokenized_text)
+
+        # Map the token strings to their vocabulary indeces
+        indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+        # Assign all tokens belonging to one sentence with a sentence id
+        segment_ids = [1]*len(tokenized_text)
+
+        # Convert inputs to Pytorch tensors
+        tokens_tensor = torch.tensor([indexed_tokens])
+        segments_tensors = torch.tensor([segment_ids])
+
+        # Load pre-trained model
+        model = BertModel.from_pretrained('bert-base-uncased',
+                                        output_hidden_states=True)
+        model.eval()
+
+        break
