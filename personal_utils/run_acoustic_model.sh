7b5d0eac69bf63fc0f93e7238bcc507252f4d35b
pepe <pepe@Speech.myguest.virtualbox.org>
2023-04-16 22:38:23 -0700

    generate acoustic model embeddings

--- personal_utils/run_acoustic_model.sh
+++ personal_utils/run_acoustic_model.sh
@@ -1,7 +1,11 @@
- model_dir=local/data/tdnn1d_sp
- dset=train
- nj=10
+model_dir=local/data/tdnn1d_sp
+dset=train
+nj=10
 
- $decode_cmd JOB=1:$nj $model_dir/log/compute_output.JOB.log \
- nnet3-compute --online-ivectors=scp:exp/nnet3_cleaned/ivectors_${dset}_hires/ivector_online.scp --online-ivector-period=10  \
- $model_dir/final.mdl scp:local/data/${dset}_hires/feats.scp ark,scp:local/data/${dset}_hires/nnet_prediction.ark,local/data/${dset}_hires/nnet_prediction.scp
+# nnet3-copy --binary=false local/data/tdnn1d_sp/final.mdl local/data/tdnn1d_sp/final_text.mdl
+
+cp personal_utils/final_golden.mdl $model_dir/final_text.mdl
+
+$decode_cmd JOB=1:$nj $model_dir/log/compute_output.JOB.log \
+nnet3-compute --online-ivectors=scp:exp/nnet3_cleaned/ivectors_${dset}_hires/ivector_online.scp --online-ivector-period=10  \
+$model_dir/final_text.mdl scp:local/data/${dset}_hires/feats.scp ark,scp:local/data/${dset}_hires/nnet_prediction.ark,local/data/${dset}_hires/nnet_prediction.scp
