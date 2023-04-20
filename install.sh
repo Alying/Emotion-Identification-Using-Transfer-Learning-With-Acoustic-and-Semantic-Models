c10baebdc32f90075ee439169dfc3e4dfd7e9634
Alina Ying <alina.ying@gmail.com>
2023-04-20 05:46:04 +0000

    updated install file after stage 5

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ sudo apt install python3-pandas
 sudo apt-get install python3-sklearn
 sudo apt install python3-pip
 # pip install tensorflow==1.11.0.
-# pip install numpy
 # pip install torch
 # pip install future
 sudo pip3 install --upgrade pip
+sudo pip3 install scipy==1.1.0
+sudo pip install numpy==1.18
 sudo pip3 install setuptools-rust
 sudo -H pip3 install --ignore-installed PyYAML
 sudo pip3 install transformers
@@ -17,4 +18,4 @@ sudo pip3 install kaldi-io
 git clone https://github.com/vesis84/kaldi-io-for-python.git
 cd kaldi-io-for-python
 sudo python3 setup.py install
-sudo pip3 install -U scikit-learn
+sudo pip3 install -U scikit-learn==0.21.3
