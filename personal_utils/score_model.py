ad348e4ac1ebfb1cd2c4ebff0d8fe5fe4e8f66e3
Alina Ying <alina.ying@gmail.com>
2023-04-27 06:36:42 +0000

    added scoring code

--- personal_utils/score_model.py
+++ personal_utils/score_model.py
@@ -5,13 +5,17 @@ dset = "train"
 count = 0
 emotion_mapping = ['ANG','DIS','FEA','HAP','NEU','SAD']
 
+total_utterances = 0
+correct_emotions = 0
+emotion_votes={}
+current_emotion_id = ""
 with open(f"local/data/{dset}_hires/predictions.ark.txt", "r") as f:
     for line in f:
         #print(line)
         emotion_values = line.split()
-        print(emotion_values)
         closest_to_zero = sys.float_info.max
         closest_to_zero_ind = -1
+        previous_emotion_id=""
 
         for i,emotion_value in enumerate(emotion_values):
             try: 
@@ -20,12 +24,26 @@ with open(f"local/data/{dset}_hires/predictions.ark.txt", "r") as f:
                     closest_to_zero = abs(emotion_val)
                     closest_to_zero_ind = i
             except ValueError:
-                # string is not a float
+                # either the emotion id or ']'
+                if len(emotion_value) > 3:
+                    # if not first utterance id, compare voted emotion to actual emotion
+                    if current_emotion_id != "":
+                        previous_emotion_id = current_emotion_id
+                        voted_emotion = max(emotion_votes, key=emotion_votes.get)
+                        #print("===actual: ",previous_emotion_id,"voted: ",voted_emotion)
+                        if previous_emotion_id == voted_emotion:
+                            correct_emotions += 1
+                           # print(correct_emotions)
+                    current_emotion_id = emotion_value[:3]
+                    emotion_votes = {'ANG':0,'DIS':0,'FEA':0,'HAP':0,'NEU':0,'SAD':0}
+                    #print(emotion_value, emotion_idi)
+                    total_utterances += 1
                 break
-        print(closest_to_zero_ind)
         emotion = emotion_mapping[closest_to_zero_ind]
-        print(emotion)
+        emotion_votes[emotion] += 1
 
-        count+=1
-        if count > 2:
-            break
+        """count+=1
+        if count > 500:
+            break"""
+
+print("accuracy: ",correct_emotions/total_utterances, "correct_emotions: ", correct_emotions, "total utterances: ", total_utterances)
