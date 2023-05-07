d8ed84ab276174fdbce4a7d7b64d1cd50d1e4d9b
pepe <pepe@Speech.myguest.virtualbox.org>
2023-05-07 12:32:43 -0700

    fixes new file syntax

--- personal_utils/run_acoustic_model.sh
+++ personal_utils/run_acoustic_model.sh
@@ -7,6 +7,7 @@ model_dir=local/data/tdnn1d_sp
 #dset=train
 nj=10
 
+sudo touch $model_dir/extract.config
 echo "output-node name=output input=tdnnf12.noop" > $model_dir/extract.config
 nnet3-copy --binary=false --nnet-config=$model_dir/extract.config $model_dir/final.mdl $model_dir/final2.raw
 
